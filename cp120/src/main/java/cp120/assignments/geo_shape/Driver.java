cd81ba44321dd2264e6c27af864f8b0f158c8d19
lajaunieste <lajaunieste@gmail.com>
2018-03-20 11:13:05 -0700

    add commenting to demo driver

--- cp120/src/main/java/cp120/assignments/geo_shape/Driver.java
+++ cp120/src/main/java/cp120/assignments/geo_shape/Driver.java
@@ -6,6 +6,7 @@ import java.awt.Color;
 public class Driver {
 
     public static void main(String[] args) {
+        /*Instantiate a bunch of shapes*/
         GeoPlane gp = new GeoPlane();
         GeoLine lineY = new GeoLine(new GeoPoint(250,0),Color.ORANGE,new GeoPoint(250,500));
         GeoLine lineX = new GeoLine(new GeoPoint(0,250),Color.GREEN,new GeoPoint(500,250));
@@ -16,15 +17,19 @@ public class Driver {
         GeoOval goCross1 = new GeoOval(new GeoPoint(242,0),Color.MAGENTA,16,500);
         GeoOval goCross2 = new GeoOval(new GeoPoint(0,242),Color.MAGENTA,500,16);
         
+        /*Configure shapes as desired*/
         lineCross1.setEdgeWidth(10);
         lineCross2.setEdgeWidth(10);
         
+        /*Add the shapes to the GeoPlane's linked list- will iterate through linked list 
+         * containing the GeoShapes*/
         GeoShape[] arrOfShapes = { lineCross1, lineCross2, goCross1, goCross2,
-                                    gr, goCtr, lineY, lineX };
-        
+                gr, goCtr, lineY, lineX };
+
         for (GeoShape shape: arrOfShapes)
             gp.addShape(shape);
         
+        /*Instantiate the drawing surface and render Shape components*/
         gp.show();
         
     }
